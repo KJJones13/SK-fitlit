fe5bdbb7e019907d2c8ffe24e79d891ff0ddc5e4
Stacy Potten <stacylynnaz@gmail.com>
2020-08-25 15:33:58 -0600

    Add getUserData method to userRepository class

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,8 +1,14 @@
 class UserRepository {
-  constructor() {
+  constructor(data) {
+    // this.users = data.map(function(user) {
+    //   return new User(user);
+    this.data = data;
+  }
+  getUserData(id) {
+    let userMatch = this.data.find(user => id === this.data.id)
+    return userMatch;
   }
 }
-
 if (typeof module !== 'undefined') {
   module.exports = UserRepository;
 }
