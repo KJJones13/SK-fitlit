8f88a8713d91eb75d7c0976369e468f1439cd6a5
Stacy Potten <stacylynnaz@gmail.com>
2020-08-25 15:47:36 -0600

    Add calculateAverageStepGoal method

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,12 +1,18 @@
 class UserRepository {
   constructor(data) {
-    // this.users = data.map(function(user) {
-    //   return new User(user);
     this.data = data;
   }
+
   getUserData(id) {
     let userMatch = this.data.find(user => id === this.data.id)
     return userMatch;
+  }
+
+  calculateAverageStepGoal() {
+    const totalStepGoal = this.data.reduce((totalSteps, info) => {
+      return totalSteps += info.dailyStepGoal
+    }, 0)
+    return totalStepGoal / this.data.length; 
   }
 }
 if (typeof module !== 'undefined') {
