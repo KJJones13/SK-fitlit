c5327e55be0254c91aba8a5651a079bc6ee39709
Kyle Jones <rmbart395@yahoo.com>
2020-08-25 20:51:03 -0600

    Add code to scripts.js to display user card info

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const userGreeting = document.querySelector('#user-name');
+const userInfo = document.querySelectorAll('.user-info-text');
+const user = new User(userData[10]);
+
+window.addEventListener('load', loadHandler);
+
+function loadHandler() {
+  greeting();
+  displayInfoCard();
+}
+
+function greeting() {
+  userGreeting.innerText = `Welcome ${user.getFirstName()}`
+};
+
+function displayInfoCard() {
+  userInfo[0].innerText = `${user.address}`
+  userInfo[1].innerText = `${user.email}`
+  userInfo[2].innerText = `${user.strideLength}`
+  userInfo[3].innerText = `${user.dailyStepGoal}`
+  userInfo[4].innerText = `${user.friends}`
+};
