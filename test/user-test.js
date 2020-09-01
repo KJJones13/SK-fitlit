45f61ada9d56bffa61c976584a2936971d283879
Stacy Potten <stacylynnaz@gmail.com>
2020-09-01 17:07:29 -0600

    Refactor user-test.js for ES6

--- test/user-test.js
+++ test/user-test.js
@@ -4,9 +4,9 @@ const User = require('../src/user');
 const userData = require('../data/user-sample-data');
 const UserRepository = require('../src/userRepository');
 
-describe('User', function() {
+describe.only('User', () => {
   let user1;
-  beforeEach(function() {
+  beforeEach(() => {
     user1 = new User({
       id: 1,
       name: 'Luisa Hane',
@@ -22,15 +22,15 @@ describe('User', function() {
     })
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(User).to.be.a('function');
   });
 
-  it('should be an instance of User', function() {
+  it('should be an instance of User', () => {
     expect(user1).to.be.an.instanceof(User);
   });
 
-  it('should take info from userData', function() {
+  it('should take info from userData', () => {
     expect(user1.id).to.equal(1);
     expect(user1.name).to.equal('Luisa Hane')
     expect(user1.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697')
@@ -40,7 +40,7 @@ describe('User', function() {
     expect(user1.friends).to.deep.equal([16, 4, 8])
   });
 
-  it('should return a users first name only', function() {
+  it('should return a users first name only', () => {
     expect(user1.getFirstName()).to.equal('Luisa');
   })
 });
