9875bc8177a91cb6a7eb66f83663b57829db886d
Stacy Potten <stacylynnaz@gmail.com>
2020-09-01 16:46:00 -0600

    Add test for sortDates method

--- test/activityRepository-test.js
+++ test/activityRepository-test.js
@@ -31,6 +31,58 @@ describe.only('Activity Repository', function() {
     expect(activityRepository.data).to.deep.equal(data);
   });
 
+  it('should sort the activityData for a user', function() {
+    expect(activityRepository.sortDates(1)).to.deep.equal([{
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numSteps": 4294,
+      "minutesActive": 138,
+      "flightsOfStairs": 10
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numSteps": 7402,
+      "minutesActive": 116,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/18",
+      "numSteps": 3486,
+      "minutesActive": 114,
+      "flightsOfStairs": 32
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/19",
+      "numSteps": 11374,
+      "minutesActive": 213,
+      "flightsOfStairs": 13
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/20",
+      "numSteps": 14810,
+      "minutesActive": 287,
+      "flightsOfStairs": 18
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/21",
+      "numSteps": 2634,
+      "minutesActive": 107,
+      "flightsOfStairs": 5
+    }]);
+  });
+
   it('should return the activity data for a given user and day', function() {
     expect(activityRepository.getDayEntry(1, '2019/06/15')).to.deep.equal({
       "userID": 1,
